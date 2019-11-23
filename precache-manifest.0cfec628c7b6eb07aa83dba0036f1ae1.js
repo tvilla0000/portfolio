c01093ead4520f4ee3a7c33faf109baaf1c2cdf5
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-11-22 17:17:07 -0800

    Updates

---
+++ precache-manifest.0cfec628c7b6eb07aa83dba0036f1ae1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93764eb240e47cd129300ba171438bc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a00cd7931edfa3924248",
+    "url": "/portfolio/static/css/main.b11c0ced.chunk.css"
+  },
+  {
+    "revision": "b29744809f482140b081",
+    "url": "/portfolio/static/js/2.526ff97e.chunk.js"
+  },
+  {
+    "revision": "a00cd7931edfa3924248",
+    "url": "/portfolio/static/js/main.9029e20f.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
